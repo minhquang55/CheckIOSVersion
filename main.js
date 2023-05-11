adffdcea5cc397d59813655a46dd0408a04a501f
tienvm1 <tienvm1@kaopiz.com>
2023-05-11 13:38:22 +0700

    Coding

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 function getiOSVersion() {
   const userAgent = navigator.userAgent;
-  const match = userAgent.match(
-    /(iPad|iPhone|iPod)\s+OS\s+(\d+_)?(\d+(\.\d+)?(\.\d+)?)/i
-  );
-  document.getElementById("detail").textContent = userAgent;
-  document.getElementById("detail1").textContent = match || 'Unmatched';
-  if (match) {
-    // Lấy phiên bản iOS từ user agent
-    const version = match[3].replace(/_/g, ".");
-    return version;
+  const match = userAgent.match(/(iPad|iPhone|iPod).*? OS (\d+)_(\d+)_?(\d+)?/);
+  if (match && match.length > 2) {
+    // Lấy phiên bản iOS từ kết quả trùng khớp
+    const majorVersion = parseInt(match[2], 10);
+    const minorVersion = parseInt(match[3], 10);
+    const patchVersion = match[4] ? parseInt(match[4], 10) : 0;
+
+    return [majorVersion, minorVersion, patchVersion];
   }
   return null; // Trả về null nếu không phải iOS
 }
 
 const iOSVersion = getiOSVersion();
 if (iOSVersion) {
-  document.getElementById(
-    "title"
-  ).textContent = `Phiên bản iOS: ${iOSVersion.join(".")}`;
+  document.getElementById("title").textContent = `Phiên bản iOS: ${iOSVersion.join('.')}`;
 } else {
   document.getElementById("title").textContent = "Không phải iOS";
 }
